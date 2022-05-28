c8d9b7b8f4a6e99d826869205a10619d52e5543b
TiveCS <creativecode.id@gmail.com>
2022-05-28 16:22:07 +0700

    Add project files.

---
+++ modul11_1302204090/Program.cs
@@ -0,0 +1,26 @@
+﻿using modul11_1302204090;
+
+PusatDataSingleton data1 = PusatDataSingleton.GetDataSingleton();
+PusatDataSingleton data2 = PusatDataSingleton.GetDataSingleton();
+
+data1.AddSebuahData("Ahmad Fathan Hanif");
+data1.AddSebuahData("Mohammad Lutfi");
+data1.AddSebuahData("Muhamad Iqbal Faturohman");
+data1.AddSebuahData("Juanda Sinurat");
+data1.AddSebuahData("Bayu Evri Saputra");
+data1.AddSebuahData("Defrizal Cahyono Putro");
+data1.AddSebuahData("Rifky Lovanto");
+
+data2.PrintSemuaData();
+data2.HapusSebuahData(data2.GetSemuaData().Count() - 1);
+
+data1.PrintSemuaData();
+
+Console.WriteLine("\nData 1: " + data1.GetSemuaData().Count());
+Console.WriteLine("Data 2: " + data2.GetSemuaData().Count());
+
+
+
+
+
+
---
+++ modul11_1302204090/PusatDataSingleton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul11_1302204090
+{
+	internal class PusatDataSingleton
+	{
+		public static PusatDataSingleton GetDataSingleton()
+		{
+			if (_instance == null)
+			{
+				_instance = new PusatDataSingleton();
+			}
+			return _instance;
+		}
+
+		private static PusatDataSingleton? _instance { get; set; }
+
+		public List<string> DataTersimpan { get; set; }
+
+		// Konstruktor private agar instance class hanya sekali dibuat
+		private PusatDataSingleton()
+		{
+			DataTersimpan = new List<string>();
+		}
+
+		public void AddSebuahData(string input)
+		{
+			DataTersimpan.Add(input);
+		}
+
+		public void HapusSebuahData(int index)
+		{
+			DataTersimpan.RemoveAt(index);
+		}
+
+		public void PrintSemuaData()
+		{
+			Console.WriteLine(" ");
+			foreach (var item in DataTersimpan)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+		public List<string> GetSemuaData()
+		{
+			return DataTersimpan;
+		}
+
+
+
+	}
+}
